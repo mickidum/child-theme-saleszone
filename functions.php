2133dcc7b85ca952b024f4c955f00d0047011754
Michael <michael@zur.local>
2019-06-10 15:26:09 +0300

    wow

--- functions.php
+++ functions.php
@@ -42,14 +42,64 @@ function saleszone_parent_theme_enqueue_styles() {
 	// 	array( 'child-theme-saleszone-bootstrap' )
 	// );
 }
+if (is_rtl()) {
 
-/**
- * Register the /wp-json/acf/v3/posts endpoint so it will be cached.
- */
-function public_woo_cache_endpoint( $allowed_endpoints ) {
-    if ( ! isset( $allowed_endpoints[ 'public-woo/v1' ] ) || ! in_array( 'products', $allowed_endpoints[ 'public-woo/v1' ] ) ) {
-        $allowed_endpoints[ 'public-woo/v1' ][] = 'products';
+// define the woocommerce_pagination_args callback 
+function filter_woocommerce_pagination_args( $arr ) { 
+    $arr['prev_text'] = '&rarr;';
+    $arr['next_text'] = '&larr;';
+
+    return $arr; 
+}; 
+         
+// add the filter 
+add_filter( 'woocommerce_pagination_args', 'filter_woocommerce_pagination_args', 10, 1 ); 
+
+function saleszone_get_comments_pagination_args()
+  {
+    return array(
+        'prev_text' => '&rarr;',
+        'next_text' => '&larr;',
+        'type'      => 'array',
+        'echo'      => false,
+    );
+  }
+
+function saleszone_post_pagination( $echo = true )
+  {
+    global  $wp_query ;
+    $total = $wp_query->max_num_pages;
+      
+    if ( $total > 1 ) {
+      $pages = paginate_links( array(
+          'current'   => max( 1, get_query_var( 'paged' ) ),
+          'total'     => $total,
+          'mid_size'  => 3,
+          'type'      => 'array',
+          'prev_text' => '→',
+          'next_text' => '←',
+      ) );
+      ob_start();
+      echo  '<ul class="pagination">' ;
+      foreach ( $pages as $page ) {
+          echo  '<li class="pagination__item">' . wp_kses( $page, array(
+              'a'    => array(
+              'class' => true,
+              'href'  => true,
+          ),
+              'span' => array(
+              'class' => true,
+          ),
+          ) ) . '</li>' ;
+      }
+      echo  '</ul>' ;
+      $html = ob_get_clean();
+      
+      if ( $echo ) {
+          echo  wp_kses_post( $html ) ;
+      } else {
+          return $html;
+      }
     }
-    return $allowed_endpoints;
+  }
 }
-add_filter( 'wp_rest_cache/allowed_endpoints', 'public_woo_cache_endpoint', 10, 1);
